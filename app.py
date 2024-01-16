4442e03e5dfdc523c849ef0a4dcb1d1cf3488c1e
mylosz01 <240840@edu.p.lodz.pl>
2024-01-16 01:13:35 +0100

    Add multithreading to read frames

--- app.py
+++ app.py
@@ -2,72 +2,64 @@ from ultralytics import YOLO
 import cv2
 import tracker as tr
 import config
+import time
+from camera import VideoCaptureThreading
 
-#camera url
-URL_CAMERA = 'test_2.mp4'#"http://192.168.0.241:8080/video"
+#camera thread object
+camera = VideoCaptureThreading(src = 'test_2.mp4', width=config.FRAME_WIDTH, height= config.FRAME_HEIGHT)
 
-# set video parameters
-FRAME_WIDTH = config.FRAME_WIDTH
-FRAME_HEIGHT = config.FRAME_HEIGHT
-FPS = config.FPS
-
-
-cap = cv2.VideoCapture(URL_CAMERA)
-if cap is None or not cap.isOpened():
+#check camera
+if camera is None or not camera.cap.isOpened():
     print('Error: camera is not available')
-
-
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,FRAME_WIDTH)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,FRAME_HEIGHT)
-cap.set(cv2.CAP_PROP_FPS,FPS)
-
-frame_count = 0
 
 #read the model
 culinaryDetector = YOLO(config.MODEL_NAME)
 tracker = tr.Tracker()
 
 print('START...')
+camera.start()
+frame_count = 0
+start_time = time.time()
 
-while cap.isOpened():
+#main loop
+while camera.cap.isOpened():
     
-    ret, frame = cap.read()
+    #read frame from capture
+    ret, frame = camera.read()
 
     if ret is not None:
-        frame_count = cap.get(cv2.CAP_PROP_POS_FRAMES)
-        frame = cv2.resize(frame,(FRAME_WIDTH,FRAME_HEIGHT))
-        
-        results = culinaryDetector.predict(frame,imgsz=(1088,736), device=0, conf=0.75, verbose=False)
-
-        img = frame.copy()
+        img = cv2.resize(frame,(config.FRAME_WIDTH,config.FRAME_HEIGHT))
+        #imgsz=(1088,736)
+        #detect objects from single frame
+        results = culinaryDetector.predict(img,imgsz=(640,480), device=0, conf=0.75, verbose=False,stream_buffer=True)
 
         #add detection line zone
-        cv2.line(img,(FRAME_WIDTH // 2,0),(FRAME_WIDTH//2,FRAME_HEIGHT),(0,255,255),8)
+        cv2.line(img,(config.FRAME_WIDTH // 2,0),(config.FRAME_WIDTH//2,config.FRAME_HEIGHT),(0,255,255),8)
 
+        #update position of objects
         tracker.update(results)
 
+        #annote detected objects
         img = tracker.annotate_objects(img)
 
+        #show statistics
         tracker.show_stats(img)
-        cv2.imshow('Culinary detect app',cv2.resize(img,(FRAME_WIDTH,FRAME_HEIGHT)))
+        #display final frame
+        cv2.imshow('Culinary detect app',img)
         
+        #fetch one frame per press
         """key = cv2.waitKey(0) & 0XFF
         if key == ord('q'):
             break"""
+    frame_count += 1
 
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-    else:
-        #return frame
-        print("Lose frame...")
-        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_count - 1)
-        cv2.waitKey(5)
-        
+    #exit loop
     if cv2.waitKey(1) == ord("q"):
-        break
+       break
 
-
+#close objects
+camera.stop()
+print(f'FPS: {(frame_count / (time.time() - start_time))}')
 print('STOP...')
-cap.release()
+camera.cap.release()
 cv2.destroyAllWindows()
